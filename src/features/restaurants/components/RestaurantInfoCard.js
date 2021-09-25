4d5cd6996341acf8b4783a573f2f56310b821fc6
josephkem <josephkemoy@gmail.com>
2021-09-25 12:47:54 -0400

    Add fonts and wired them to the main app

--- src/features/restaurants/components/RestaurantInfoCard.js
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -8,6 +8,7 @@ const RestaurantCard = styled(Card)`
 `;
 
 const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body}
   padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
