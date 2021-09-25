33285fe3beb190777a7b783e21680427a1f2137f
josephkem <josephkemoy@gmail.com>
2021-09-25 12:28:26 -0400

    Installed fonts for the app

--- src/features/restaurants/components/RestaurantInfoCard.js
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
--- src/features/restaurants/screens/RestaurantScreen.js
+++ src/features/restaurants/screens/RestaurantScreen.js
@@ -10,7 +10,7 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
