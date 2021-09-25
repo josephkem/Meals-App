62cc245a4e0955114c4e901222c519d4dc51944d
josephkem <josephkemoy@gmail.com>
2021-09-24 20:04:05 -0400

    Added react native paper to the project

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatucBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({});
