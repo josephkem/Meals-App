55de7037bd45c8a889388e7ccc9da194a401f139
josephkem <josephkemoy@gmail.com>
2021-10-22 10:57:24 -0400

    Installed package to use svg for rating and added dummy data to test out the card

--- src/features/restaurants/components/RestaurantInfoCard.js
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components/native";
 import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -22,12 +24,22 @@ font-family: ${(props) => props.theme.fonts.body}
 font-size: ${(props) => props.theme.fontSizes.caption}
 `;
 
-function RestaurantinfoCard() {
+function RestaurantInfoCard({ restaurant = {} }) {
+  const {
+    name = "Some Restaurantt",
+    icon,
+    photos = [],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
   return (
     <RestaurantCard elevation={5} style={styles.card}>
       <Info>
-        <Title>Name</Title>
-        <Address>300 Elm Street</Address>
+        <Title>{name}</Title>
+        <SvgXml xml={star} width={20} height={20} />
+        <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   );
@@ -40,4 +52,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default RestaurantinfoCard;
+export default RestaurantInfoCard;
